61e3ee8d146a04df9b1c5b7a7a00e7f945edc9f7
gujiu6 <18273655445@163.com>
2026-04-14 20:13:05 +0800

    4.14

--- Graph/LCA(BinaryLifting).cpp
+++ Graph/LCA(BinaryLifting).cpp
@@ -3,9 +3,19 @@ using namespace std;
 const int MAXN=5e5+10,MAXM=5e5+10,LIMIT=20;
 
 
+
+
+int log2(int n) {
+    int ans = 0;
+    while ((1 << ans) <= (n >> 1)) {
+        ans++;
+    }
+    return ans;
+}
+
 int cnt,lg2;
 array<int,MAXN>h,deep;
-array<int,MAXM>nxt,to,wei;
+array<int,MAXM<<1>nxt,to,wei;
 array<array<int,LIMIT>,MAXN>stjump;
 
 void build(int n){
@@ -25,20 +35,17 @@ void addEdge(int u,int v,int w=0){
 void dfs(int u,int f){
     deep[u]=deep[f]+1;
     stjump[u][0]=f;
-    for(int p=1;p<=deep[u];p++){
+    for(int p=1;p<=lg2;p++){
         stjump[u][p]=stjump[stjump[u][p-1]][p-1];
     }
-    for(int ei=h[u],v;ei>0;ei=nxt[ei]){
-        v=to[ei];
-        if(v==f) continue;
-        dfs(v,u);
+    for(int ei=h[u];ei>0;ei=nxt[ei]){
+        if(to[ei]!=f)
+            dfs(to[ei],u);
     }
 }
 
 int lca(int a,int b){
-    if(deep[a]<deep[b]){
-        swap(a,b);
-    }
+    if(deep[a]<deep[b]) swap(a,b);
     for(int p=lg2;p>=0;p--){
         if(deep[stjump[a][p]]>=deep[b]){
             a=stjump[a][p];
