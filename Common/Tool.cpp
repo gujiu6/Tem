4b432878ef8ab5ac10a511cd0e90a829ebb02963
故旧 <18273655445@163.com>
2026-05-31 00:29:27 +0800

    111

--- Common/Tool.cpp
+++ Common/Tool.cpp
@@ -8,7 +8,36 @@ typedef long long ll;
 
 
 
+//快速幂
+ll power(ll a,ll b,ll MOD=1e9+7){
+	ll ans=1;
+	a%=MOD;
+	while(b>0){
+		if(b&1) ans=(ans*a)%MOD;
+		a=(a*a)%MOD;
+		b>>=1;
+	}
+	return ans;
+}
 
+ll getinv(int n,int MOD=1e9+7){
+    return power(n,MOD-2,MOD);
+}
+
+//整数开方
+ll my_sqrt(ll a)
+{
+    ll l=0,r=5e9+10;
+    while(r-l>1)
+    {
+        ll mid=(l+r)/2;
+        if(1ll*mid*mid<=a)l=mid;
+        else r=mid;
+    }
+    return l;
+}
+
+//快读
 ll read(){
     bool flag=false;
     ll ans=0;
@@ -23,7 +52,7 @@ ll read(){
     }
     return flag?-ans:ans;
 }
-
+//快写
 void write(ll x){
     if(x==0){
         putchar('0');
@@ -43,6 +72,3 @@ void write(ll x){
         putchar(ch[i]);
     }
 }
-
-
-
