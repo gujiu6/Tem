11a866bc2ffca9bb685807b5d6c81518034efa2d
故旧 <18273655445@163.com>
2026-05-31 00:30:58 +0800

    11

--- Math/C(n,m).cpp
+++ Math/C(n,m).cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MOD=1e9+7;
+const int MOD=1e9+7 ,MAXX=2e5;
 typedef long long ll;
 
 
@@ -50,7 +50,21 @@ public:
 };
 
 
+array<array<ll,MAXX+1>,MAXX+1>C,pre;
 
+
+void CC(){
+	for(int n=0;n<=MAXX;n++){
+		C[n][0]=C[n][n]=1;
+		for(int k=1;k<n;k++){
+			C[n][k]=(C[n-1][k-1]+C[n-1][k])%MOD;
+		}
+		pre[n][0]=C[n][0];
+		for(int k=1;k<=n;k++){
+			pre[n][k]=(pre[n][k-1]+C[n][k])%MOD;
+		}
+	}
+}
 
 
 
