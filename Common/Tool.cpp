0a451c23e712c8df79f336150ac944eee69e829b
故旧 <18273655445@163.com>
2026-06-04 20:38:48 +0800

    11

--- Common/Tool.cpp
+++ Common/Tool.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXX=2e5;
+const int MAXX=2e5, MOD = 1e9+7;
 typedef long long ll;
 
 
@@ -19,10 +19,37 @@ ll power(ll a,ll b,ll MOD=1e9+7){
 	}
 	return ans;
 }
-
+//费马小
 ll getinv(int n,int MOD=1e9+7){
     return power(n,MOD-2,MOD);
 }
+
+//矩阵快速幂
+vector<vector<ll>>mul(const vector<vector<ll>>&a,const vector<vector<ll>>&b){
+    int n=a.size();
+    int k=a[0].size();
+    int m=b[0].size();
+    vector<vector<ll>>ans(n,vector<ll>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            for(int c=0;c<k;c++){
+                ans[i][j]+=a[i][c]*b[c][j];
+            }
+            ans[i][j]%=MOD;
+        }
+    }
+    return ans;
+}
+vector<vector<ll>>power(vector<vector<ll>>a,int p,const vector<vector<ll>>&f){
+    vector<vector<ll>>ans=f;
+    while(p>0){
+        if(p&1) ans=mul(a,ans);
+        a=mul(a,a);
+        p>>=1;
+    }
+    return ans;
+}
+
 
 //整数开方
 ll my_sqrt(ll a)
