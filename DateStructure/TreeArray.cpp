567d22895192b621961b244494ba937c0f98d6e6
故旧 <18273655445@163.com>
2026-06-04 20:26:43 +0800

    111

--- DateStructure/TreeArray.cpp
+++ DateStructure/TreeArray.cpp
@@ -7,46 +7,46 @@ typedef long long ll;
 
 
 
-template <typename T=ll>
-class BIT{
+template <typename T = ll>
+class BIT {
 private:
     int n;
     vector<T> tree;
 
-    inline int lowbit(int i){
-        return i&-i;
+    inline int lowbit(int i) {
+        return i & -i;
     }
 public:
-    BIT(int n):n(n),tree(n+1,0){}
-    BIT(vector<T>& arr,int n):BIT(n){
-        for(int i=1; i<=n;i++) {
-            tree[i]+=arr[i];
-            int j=i+lowbit(i);
-            if(j<=n) tree[j]+=tree[i];
+    BIT(int n) : n(n), tree(n + 1, 0) {}
+    BIT(vector<T>& arr, int n) : BIT(n) {
+        for(int i = 1; i <= n; i++) {
+            tree[i] += arr[i];
+            int j = i + lowbit(i);
+            if(j <= n) tree[j] += tree[i];
         }
     }
-    void add(int i,T v){
-        while(i<=n){
-            tree[i]+=v;
-            i+=lowbit(i);
+    void add(int i, T v) {
+        while(i <= n){
+            tree[i] += v;
+            i += lowbit(i);
         }
     }
 
-    T qry(int i){
-        T ans=0;
-        while(i>0){
-            ans+=tree[i];
-            i-=lowbit(i);
+    T qry(int i) {
+        T ans = 0;
+        while(i > 0) {
+            ans += tree[i];
+            i -= lowbit(i);
         }
         return ans;
     }
 
-    T range_qry(int left,int right){
-        return qry(right)-qry(left-1);
+    T range_qry(int left, int right) {
+        return qry(right) - qry(left - 1);
     }
 
-    void clear(){
-        fill(tree.begin(),tree.end(),0);
+    void clear() {
+        fill(tree.begin(), tree.end(), 0);
     }
 };
 
