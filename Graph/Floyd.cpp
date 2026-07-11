3aa0c217102e0870fec6091d61c9b8a17c5f3396
故旧 <18273655445@163.com>
2026-07-12 00:23:47 +0800

    11

--- Graph/Floyd.cpp
+++ Graph/Floyd.cpp
@@ -8,12 +8,12 @@ const int INF=1e9+7,MAXX=100;
 vector<vector<int>>dis(MAXX,vector<int>(MAXX,INF));
 
 
-void floyd(int n){
-    for(int k=1;k<=n;k++){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(dis[i][k]!=INF&&dis[k][j]!=INF&&dis[i][j]>dis[i][k]+dis[k][j])
-                    dis[i][j]=dis[i][k]+dis[k][j];
+void Floyd(int n){
+    for(int k = 1;k <= n; k++){
+        for(int i = 1;i <= n;  i++){
+            for(int j = 1; j <= n; j++){
+                if(dis[i][k] != INF && dis[k][j] != INF && dis[i][j] > dis[i][k] + dis[k][j])
+                    dis[i][j] = dis[i][k] + dis[k][j];
             }
         }
     }
