a5147e61f180c47d547523e1409f383191e20e01
gujiu6 <18273655445@163.com>
2026-07-19 11:16:33 +0800

    11

--- Math/Comb/C(n,m).cpp
+++ Math/Comb/C(n,m).cpp
@@ -6,6 +6,8 @@ typedef long long ll;
 
 
 
+//组合数
+namespace CC1{
 
 class CC{
 private:
@@ -33,7 +35,7 @@ public:
     }
     ll C(int n, int m){
         if(m < 0 || m > n) return 0;
-        return f[n] * inv[m] % mod * inv[n-m] % mod;
+        return f[n] * inv[m] % mod * inv[n - m] % mod;
     }
     ll P(int n, int m){
         if(m < 0 || m > n) return 0;
@@ -50,11 +52,60 @@ public:
     }
 };
 
+}
+
+namespace CC2{
+
+class CC{
+private:
+    vector<ll>f, inv;
+    int mod;
+public:
+    ll power(ll a, ll b){
+        ll ans = 1;
+        a %= mod;
+        while(b > 0){
+            if(b & 1) ans = (ans * a) % mod;
+            a = (a * a) % mod;
+            b >>= 1;
+        }
+        return ans;
+    }
+    CC(int mod) : f(mod + 1, 1), inv(mod + 1, 0), mod(mod){
+        for(int i = 1; i < mod; i++){
+            f[i] = f[i-1] * i % mod;
+        }
+        inv[mod - 1] = power(f[mod], mod - 2);
+        for(int i = mod - 2; i >= 0; i--){
+            inv[i] = inv[i + 1] * (i + 1) % mod;
+        }
+    }
+    ll C(int n, int m){
+        if(m < 0 || m > n) return 0;
+        return f[n] * inv[m] % mod * inv[n - m] % mod;
+    }
+    int lucas(int n, int m) {
+    if(m == 0) return 1;
+        return lucas(n / mod, m / mod) * C(n % mod, m % mod) % mod;
+    }
+    ll prefix_sum_C(int n, int m) {
+        if (m < 0) return 0;
+        if (m >= n) return power(2,n);
+        ll sum = 0;
+        for (int k = 0; k <= m; k++) {
+            sum = (sum + C(n, k)) % mod;
+        }
+        return sum;
+    }
+};
+
+}
+
 
 
 //预处理11
 array<array<ll, MAXX+1>, MAXX+1> C, pre;
-void CC(){
+void init(){
 	for(int n = 0;n <= MAXX;n++){
 		C[n][0]=C[n][n]=1;
 		for(int k = 1; k < n; k++){
