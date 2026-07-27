31d5a2170bdef1a4b1ff94e90f3b9f08114b3394
gujiu6 <18273655445@163.com>
2026-07-27 20:24:21 +0800

    111

--- Graph/Topological.cpp
+++ Graph/Topological.cpp
@@ -1,31 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN=1000,MAXM=2e5+10;
+const int MAXX=1000;
 typedef long long ll;
 
 
-
-
-
-
-
-
-array<int, MAXN>head, inDeg, outDeg;
-array<int, MAXM<<1>nxt, to;
+array<int, MAXX> h, inDeg, outDeg;
+array<int, MAXX << 1> nxt, to;
 int cnt;
 priority_queue<int, vector<int>, greater<int>>q;
 
 void build(){
     cnt = 1;
-    head.fill(0);
+    h.fill(0);
     inDeg.fill(0);
     outDeg.fill(0);
 }
 
 void addedge(int u, int v){
-    nxt[cnt] = head[u];
+    nxt[cnt] = h[u];
     to[cnt] = v;
-    head[u] = cnt++;
+    h[u] = cnt++;
     inDeg[v]++;
     outDeg[u]++;
 }
@@ -41,7 +35,7 @@ vector<int>topsort(int n){//字典序
         cur = q.top();
         ans.push_back(cur);
         q.pop();
-        for(int ei = head[cur]; ei != 0; ei = nxt[ei]){
+        for(int ei = h[cur]; ei != 0; ei = nxt[ei]){
             if(--inDeg[to[ei]] == 0) q.push(to[ei]);
         }
     }
