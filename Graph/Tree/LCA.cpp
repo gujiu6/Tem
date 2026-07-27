bef55c51c616b0301a91a10ac691a7aa9660e1dc
gujiu6 <18273655445@163.com>
2026-07-27 20:20:07 +0800

    111

--- Graph/Tree/LCA.cpp
+++ Graph/Tree/LCA.cpp
@@ -4,6 +4,7 @@ const int MAXX = 5e5+10, LIMIT = 20;
 
 
 
+namespace BinaryLifting {
 
 int cnt, lg2, n;
 array<int,MAXX> h, deep;
@@ -57,3 +58,53 @@ int dist(int u, int v) {
     return deep[u] + deep[v] - 2 * deep[lca(u, v)];
 }
 
+}
+
+namespace Tarjan {
+
+int cnt, n, m;
+array<int, MAXX> h, fa, vis;
+array<int, MAXX << 1> nxt, to, wei;
+vector<vector<array<int, 2>>> Question(MAXX);
+vector<int> lca;
+
+void build(){
+    cnt = 1;
+    fill(h.begin(), h.begin() + n + 1, 0);
+    fill(vis.begin(), vis.begin() + n + 1, 0);
+    fill(lca.begin(), lca.begin() + m + 1, 0);
+    for(int i = 1; i <= n; i++) 
+        fa[i] = i;
+}
+
+void addEdge(int u, int v, int w=0){
+    nxt[cnt] = h[u];
+    to[cnt] = v;
+    wei[cnt] = w;
+    h[u] = cnt++;
+}
+
+int find(int i){
+    if(i != fa[i]){
+        fa[i] = find(fa[i]);
+    }
+    return fa[i];
+}
+
+void tarjan(int u, int f){
+    vis[u] = 1;
+    for(int ei = h[u], v; ei > 0; ei = nxt[ei]){
+        v = to[ei];
+        if(v != f){
+            tarjan(v, u);
+            fa[v] = u;
+        }
+    }
+    for(auto [v, id] : Question[u]){
+        if(vis[v]){
+            lca[id] = find(v);
+        }
+    }
+}
+
+}
