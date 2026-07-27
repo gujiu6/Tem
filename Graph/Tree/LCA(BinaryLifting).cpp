97b4694d9bce0ffa32fe7b4d0e609ef37e92d08a
gujiu6 <18273655445@163.com>
2026-07-27 16:06:32 +0800

    111

--- Graph/Tree/LCA(BinaryLifting).cpp
+++ Graph/Tree/LCA(BinaryLifting).cpp
@@ -1,53 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXX=5e5+10 ,LIMIT=20;
+const int MAXX = 5e5+10, LIMIT = 20;
 
 
 
 
-int cnt,lg2;
-array<int,MAXX>h,deep;
-array<int,MAXX<<1>nxt,to,wei;
-array<array<int,LIMIT>,MAXX>stjump;
+int cnt, lg2;
+array<int,MAXX> h, deep;
+array<int,MAXX << 1> nxt, to, wei;
+array<array<int, LIMIT>, MAXX> stjump;
 
 void build(int n){
-    cnt=1;
-    lg2=__lg(n);
-    fill(h.begin(),h.begin()+n+1,0);
-    fill(deep.begin(),deep.begin()+n+1,0);
+    cnt = 1;
+    lg2 = __lg(n);
+    fill(h.begin(), h.begin() + n + 1, 0);
+    fill(deep.begin(), deep.begin() + n + 1, 0);
 }
 
-void addEdge(int u,int v,int w=0){
-    nxt[cnt]=h[u];
-    to[cnt]=v;
-    wei[cnt]=w;
-    h[u]=cnt++;
+void addEdge(int u, int v, int w = 0){
+    nxt[cnt] = h[u];
+    to[cnt] = v;
+    wei[cnt] = w;
+    h[u] = cnt++;
 }
 
-void dfs(int u,int f){
-    deep[u]=deep[f]+1;
-    stjump[u][0]=f;
-    for(int p=1;p<=lg2;p++){
-        stjump[u][p]=stjump[stjump[u][p-1]][p-1];
+void dfs(int u, int f){
+    deep[u] = deep[f] + 1;
+    stjump[u][0] = f;
+    for(int p = 1; p <= lg2; p++){
+        stjump[u][p] = stjump[stjump[u][p - 1]][p - 1];
     }
-    for(int ei=h[u];ei>0;ei=nxt[ei]){
-        if(to[ei]!=f)
-            dfs(to[ei],u);
+    for(int ei = h[u];ei > 0; ei = nxt[ei]){
+        if(to[ei] != f)
+            dfs(to[ei], u);
     }
 }
 
-int lca(int a,int b){
-    if(deep[a]<deep[b]) swap(a,b);
-    for(int p=lg2;p>=0;p--){
-        if(deep[stjump[a][p]]>=deep[b]){
-            a=stjump[a][p];
+int lca(int a, int b){
+    if(deep[a] < deep[b]) swap(a, b);
+    for(int p = lg2; p >= 0; p--){
+        if(deep[stjump[a][p]] >= deep[b]){
+            a = stjump[a][p];
         }
     }
-    if(a==b) return a;
-    for(int p=lg2;p>=0;p--){
-        if(stjump[a][p]!=stjump[b][p]){
-            a=stjump[a][p];
-            b=stjump[b][p];
+    if(a == b) return a;
+    for(int p = lg2; p >= 0; p--){
+        if(stjump[a][p] != stjump[b][p]){
+            a = stjump[a][p];
+            b = stjump[b][p];
         }
     }
     return stjump[a][0];
