e6d96762f89d892282e1d1ed672def68b5f73f8c
gujiu6 <18273655445@163.com>
2026-07-30 21:11:13 +0800

    111

--- DateStructure/BIT.cpp
+++ DateStructure/BIT.cpp
@@ -10,14 +10,14 @@ typedef long long ll;
 template <typename T = ll>
 class BIT {
 private:
-    int n;
+    int n, lg2;
     vector<T> tree;
 
     inline int lowbit(int i) {
         return i & -i;
     }
 public:
-    BIT(int n) : n(n), tree(n + 1, 0) {}
+    BIT(int n) : n(n), tree(n + 1, 0), lg2(__lg(n)) {}
     BIT(vector<T>& arr, int n) : BIT(n) {
         for(int i = 1; i <= n; i++) {
             tree[i] += arr[i];
@@ -39,6 +39,19 @@ public:
             i -= lowbit(i);
         }
         return ans;
+    }
+
+    T kth(int k) {
+        T pos = 0;
+        int pw = 1 << lg2;
+        for(int p = pw; p > 0; p >>= 1) {
+            int nxt = pos + p;
+            if(nxt <= n && tree[nxt] < k) {
+                pos = nxt;
+                k -= tree[nxt];
+            }
+        }
+        return pos + 1;
     }
 
     T range_qry(int left, int right) {
