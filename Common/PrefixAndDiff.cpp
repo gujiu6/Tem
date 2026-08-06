fdc1732f469df823b399adc0ced8f5e644c18940
gujiu6 <18273655445@163.com>
2026-08-07 02:51:30 +0800

    11

--- Common/PrefixAndDiff.cpp
+++ Common/PrefixAndDiff.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using i64 = long long;
 
-//二维前缀和
+//1.二维前缀和
 template <class T = i64>
 class Prefix {
     int n, m;
@@ -20,6 +20,7 @@ class Prefix {
     }
 };
 
+//2.二维差分
 template <class T = i64>
 class Diff {
     int n, m;
@@ -41,5 +42,83 @@ class Diff {
             }
         }
         return a;
+    }
+};
+
+//3.k阶差分
+template <class T = i64>
+vector<T> kthDiff(vector<T> a, int k) {
+    assert(k >= 0);
+    while(k--) {
+        for(int i = 1; i < a.size() - 1; i++) {
+            a[i] = a[i + 1] - a[i];
+        }
+        if(!a.empty()) {
+            a.pop_back();
+        }
+    }
+    return a;
+}
+
+//4.Kadane求非空最大子段和, 
+template <class T = i64>
+T maxSub(const vector<T>& a) {
+    assert(!a.empty());
+    auto ans = a[1], cur = a[1];
+    for(int i = 2; i < a.size(); i++) {
+        cur = max(a[i], cur + a[i]);
+        ans = max(ans, cur);
+    }
+    return ans;
+}
+//子矩阵和
+template <class T = i64>
+T maxSubMat(const vector<vector<T>>& a) {
+    assert(!a.empty() && !a[0].empty());
+    int n = a.size() - 1, m = a[0].size();
+    auto ans = a[1][1];
+    for(int u = 1; u <= n; u++) {
+        vector<T> s(m + 1);
+        for(int d = u; d <= n; d++) {
+            for(int j = 1; j <= m; j++) {
+                s[j] += a[d][j];
+            }
+            ans = max(ans, maxSub(s));
+        }
+    }
+    return ans;
+}
+
+//5.所有区间 gcd
+template <class T = i64>
+class gcdRanges {
+    int n;
+    vector<vector<pair<int, T>>> g;
+    gcdRanges (const vector<T>& a): n(a.size() - 1), g(n + 1) {
+        vector<pair<int, T>> pre;
+        for(int r = 1; r <= n; r++) {
+            vector<pair<int, T>> cur{{r, abs(a[r])}};
+            for(auto[l, x] : pre) {
+                x = gcd(x, abs(a[r]));
+                if(cur.back().second == x) {
+                    cur.back().first = l;
+                }
+                else {
+                    cur.push_back({l, x});
+                }
+            }
+            reverse(cur.begin(), cur.end());
+            g[r] = cur;
+            pre = move(cur);
+            reverse(pre.begin(), pre.end());
+        }
+    }
+    //[l, r]区间gcd
+    T qry(int l, int r) const {
+        assert(1 <= l && l <= r && r <= n);
+        auto it = upper_bound(g[r].begin(), g[r].end(), l, [](int x, const auto& p){
+            return x < p.first;
+        });
+        return prev(it)->second;
     }
 };
