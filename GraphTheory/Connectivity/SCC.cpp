af32e65525d14d64b008d351fe01c7b1993239f7
gujiu6 <18273655445@163.com>
2026-08-09 12:52:12 +0800

    11

--- GraphTheory/Connectivity/SCC.cpp
+++ GraphTheory/Connectivity/SCC.cpp
@@ -9,7 +9,7 @@ struct WEdge {int v;i64 w = 0;};struct DEdge {int u, v;i64 w = 0;};struct Edge {
 //1.Tarjan 强连通分量
 vector<int> TarjanSCC(const vector<vector<Edge>>& g) {
     int n = g.size() - 1;
-    int tim = 0, cc = 1;//时间戳,SCC编号
+    int tim = 0, cc = 0;//时间戳,SCC编号
     vector<int> dfn(n + 1), st;
     vector<int> low(n + 1);//从u出发,能够回到的最小dfn编号
     vector<int> bel(n + 1, -1);//答案数组
@@ -28,13 +28,13 @@ vector<int> TarjanSCC(const vector<vector<Edge>>& g) {
             }
         }
         if(low[u] != dfn[u]) return;
+        cc++;
         while(true) {
             int v = st.back();st.pop_back();
             on[v] = false;
             bel[v] = cc;
             if(v == u) break;
         }
-        cc++;
     };
     for(int u = 1; u <= n; u++) {
         if(!dfn[u]) {
