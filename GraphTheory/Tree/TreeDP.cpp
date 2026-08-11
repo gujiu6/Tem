f8cdee9c840b1a754b8a0aa25316221e71e6d28c
gujiu6 <18273655445@163.com>
2026-08-12 02:08:00 +0800

    1

--- GraphTheory/Tree/TreeDP.cpp
+++ GraphTheory/Tree/TreeDP.cpp
@@ -1,8 +1,10 @@
-/*
-1.树上背包基础版
+/*                                   时间复杂度         空间复杂度                             
+1.树上背包基础版                       O(nk²)             O(nk)
 2.单调队列优化树上背包
-3.长链剖分 DP 合并
-4.树上依赖背包:每件物品有父子依赖，选任一非根物品前必须先选其父物品
+3.长链剖分 DP 合并 
+4.树上依赖背包:         
+    4.1普通版本                       O(nk²)             O(nk)
+    4.2 dfn序最优                     O(nk)              O(nk)
 */
 #include <bits/stdc++.h>
 #include <cassert>
@@ -46,18 +48,19 @@ vector<T> TreePack(const vector<vector<Edge>>& g, const vector<T>& val, int k, i
 
 //3.长链剖分 DP 合并
 
-//4.树上依赖背包:每件物品有父子依赖，选任一非根物品前必须先选其父物品
+//4.树上依赖背包:每件物品有父子依赖,选任一非根物品前必须先选其父物品
+//4.1普通版本
 template <class T = i64>
 vector<T> TreeDepPack(const vector<int>& w, const vector<T>& val, const vector<int>& p, int cap) {
     // p[i]=0: i 是根节点, 返回:总容量恰好为 c 时的最大价值。
     int n = w.size() - 1;
-    vector<vector<int>> son(n + 1);
+    vector<vector<Edge>> son(n + 1);
     for(int u = 1; u <= n; u++) {
         assert(w[u] > 0);
         assert(0 <= p[u] && p[u] <= n);
-        son[p[u]].push_back(u);
+        son[p[u]].push_back({u});
     }
-    auto dfs = [&](auto &&self, int u)->vector<int> {
+    auto dfs = [&](auto &&self, int u)->vector<T> {
         vector<T> dp(cap + 1, -INF);
         //虚根
         if(u == 0) {
@@ -68,15 +71,15 @@ vector<T> TreeDepPack(const vector<int>& w, const vector<T>& val, const vector<i
             if(w[u] <= cap) 
                 dp[w[u]] = val[u];
         }
-        for(int &v : son[u]) {
-            auto h = self(self, v);
+        for(const auto &e : son[u]) {
+            auto h = self(self, e.v);
             //不选这个儿子
             auto ndp = dp;
             for(int i = 0; i <= cap; i++){
                 if(dp[i] == -INF) continue;
                 for(int j = 1; i + j <= cap; j++) {
                     if(h[j] == -INF) continue;
-                    ndp[i + j] = max(dp[i + j], dp[i] + h[j]);
+                    ndp[i + j] = max(ndp[i + j], dp[i] + h[j]);
                 }
             }
             dp = move(ndp);
@@ -84,4 +87,39 @@ vector<T> TreeDepPack(const vector<int>& w, const vector<T>& val, const vector<i
         return dp;
     };
     return dfs(dfs, 0);
+}
+//4.2 dfn序最优
+template <class T = i64>
+T TreeDepPack(const vector<int>& w, const vector<T>& val, const vector<int>& p, int cap) {
+    int n = w.size() - 1;
+    vector<vector<Edge>> son(n + 1);
+    for(int u = 1; u <= n; u++) {
+        son[p[u]].push_back({u});
+    }
+    vector<int> dfn(n + 1), dfn_w(n + 2), dfn_sz(n + 2);
+    vector<T> dfn_val(n + 2);
+    int tim = 0;
+    auto dfs = [&](auto &&self, int u)->void {
+        dfn[u] = ++tim;
+        dfn_w[dfn[u]] = w[u];
+        dfn_val[dfn[u]] = val[u];
+        dfn_sz[dfn[u]] = 1;
+        for(const auto &e : son[u]) {
+            self(self, e.v);
+            dfn_sz[dfn[u]] += dfn_sz[dfn[e.v]]; 
+        }
+    };
+    dfs(dfs, 0);
+    vector<vector<T>> dp(n + 3, vector<T>(cap + 1));
+    for(int i = n + 1; i >= 2; i--) {
+        for(int j = 0; j <= cap; j++) {
+            //不选当前节点
+            dp[i][j] = dp[i + dfn_sz[i]][j];
+            //选当前节点
+            if(j >= dfn_w[i]) {
+                dp[i][j] = max(dp[i][j], dp[i + 1][j - dfn_w[i]] + dfn_val[i]);
+            }
+        }
+    }
+    return dfn_val[1] + dp[2][cap];
 }
