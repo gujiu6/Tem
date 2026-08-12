57ba3fb0f0b3a06f3660a19a2432bbb7aaed0e1a
gujiu6 <18273655445@163.com>
2026-08-12 14:18:16 +0800

    11

--- DateStructure/SegmentTree/SegmentTree1.cpp
+++ DateStructure/SegmentTree/SegmentTree1.cpp
@@ -18,6 +18,48 @@ struct Sum {
 template <class Info>
 struct Seg {
     int n;
-    vector<Info>  tr;
-    Seg(int n = 0): n(n), 
+    vector<Info> tr;
+    Seg(int n = 0) {init(n);}
+    Seg(const vector<Info>& a){init(a);}
+    void init(const int& _n) {
+        n = _n;
+        tr.assign((n << 1) + 2, Info{});
+    }
+    void init(const vector<Info>& a) {
+        n = a.size() - 1;
+        tr.assign((n << 1) + 2, Info{});
+        for(int i = 1; i <= n; i++) {
+            tr[n + i - 1] = a[i];
+        }
+        for(int i = n - 1; i >= 1; i--) {
+            pull(i);
+        }
+    }
+    void pull(int p) {
+        tr[p] = tr[p << 1] + tr[p << 1 | 1];
+    }
+    void set(int p, Info v) {
+        p = p + n - 1;
+        tr[p] = v;
+        for(p >>= 1; p; p >>= 1) {
+            pull(p);
+        }
+    }
+    // 查询闭区间 [l, r]
+    Info qry(int l, int r) const {
+        Info x{}, y{};
+        l = l + n - 1;
+        r = r + n;
+        while(l < r) {
+            if(l & 1) {
+                x = x + tr[l++];
+            }
+            if(r & 1) {
+                y = tr[--r] + y ;
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        return x + y;
+    }
 };
