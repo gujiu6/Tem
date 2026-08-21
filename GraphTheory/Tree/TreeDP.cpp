64461e0c2f3aaace20dd667ae49395e23b63cec1
gujiu6 <18273655445@163.com>
2026-08-21 18:58:16 +0800

    11

--- GraphTheory/Tree/TreeDP.cpp
+++ GraphTheory/Tree/TreeDP.cpp
@@ -47,17 +47,17 @@ vector<T> TreePack(const vector<vector<Edge>>& g, const vector<T>& val, int k, i
 
 
 //3.长链剖分 DP 合并
-vector<int> depthMode(const vector<vector<Edge>>& g, int root = 1){
-    int n = (int)g.size() - 1;
+template <class T = i64>
+vector<T> depthMode(const vector<vector<Edge>>& g, int root = 1){
+    int n = g.size() - 1;
     vector<int> len(n + 1, 1), son(n + 1);
-    vector<int> at1(n + 1), dp1(n + 1);
-    vector<int> at2(n + 1), dp2(2 * n + 5);
-    vector<int> ans(n + 1);//u的子树中,距离u为多少的节点最多,并且如果有多个深度节点数相同,取最小的那个深度
+    vector<int> at1(n + 1), at2(n + 1);
+    vector<T> dp1(n + 5), dp2(4 * n + 5);
+    vector<T> ans;
     //求最长链
     auto dfs1 = [&](auto&& self, int u, int f) -> void{
         for (const auto &e : g[u]) {
-            if (e.v == f)
-                continue;
+            if (e.v == f) continue;
             self(self, e.v, u);
             if (len[e.v] + 1 > len[u]) {
                 len[u] = len[e.v] + 1;
@@ -67,8 +67,8 @@ vector<int> depthMode(const vector<vector<Edge>>& g, int root = 1){
     };
     dfs1(dfs1, root, 0);
     //给轻儿子分配 DP 空间
-    int ptr1 = len[root]; at1[root] = 0;
-    int ptr2 = len[root] - 1; at2[root] = len[root] - 1;
+    int ptr1 = len[root], ptr2 = 2 * len[root] + 5;
+    at1[root] = 0; at2[root] = len[root];
     //DP
     auto dfs2 = [&](auto&& self, int u, int f) -> void {
         int h = son[u];
@@ -77,30 +77,18 @@ vector<int> depthMode(const vector<vector<Edge>>& g, int root = 1){
             at1[h] = at1[u] + 1;
             at2[h] = at2[u] - 1;
             self(self, h, u);
-            //长儿子的答案转换成 u 的相对深度
-            ans[u] = ans[h] + 1;
         }
         dp1[at1[u]] = 1;//u自己,距离u为 0
-        //如果当前最大值只有1,最小深度显然是0
-        if (dp1[at1[u] + ans[u]] == 1) {
-            ans[u] = 0;
-        }
         //合并轻儿子
         for (const auto &e : g[u]){
             if (e.v == f || e.v == h) continue;
             //给轻儿子单独分配空间
             at1[e.v] = ptr1; ptr1 += len[e.v];
-            at2[e.v] = ptr2; ptr2 += len[e.v] + 1;
+            at2[e.v] = ptr2 + len[e.v]; ptr2 += 2 * len[e.v] + 5;
             self(self, e.v, u);
             // dp1[u][d] += dp1[v][d - 1]
             for (int d = 1; d <= len[e.v]; d++) {
                 dp1[at1[u] + d] += dp1[at1[e.v] + d - 1];
-                int cur = dp1[at1[u] + d];
-                int best = dp1[at1[u] + ans[u]];
-                //次数更多,或者次数相同但深度更小
-                if (cur > best || (cur == best && d < ans[u])){
-                    ans[u] = d;
-                }
             }
             // dp2[u][d] += dp2[v][d + 1]
             for(int d = 0; d < len[e.v]; d++) {
