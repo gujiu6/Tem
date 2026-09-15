ce953acdaf61c0ba6f598bb2e94e763eee2d78b7
gujiu6 <18273655445@163.com>
2026-09-15 10:19:38 +0800

    11

--- DateStructure/BIT/BIT.cpp
+++ DateStructure/BIT/BIT.cpp
@@ -1,8 +1,10 @@
 /*
 1.单点修改/赋值,区间/前缀和
 2.区间修改, 区间和
+3.二维树状数组
 */
 #include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
 const int MAXX=2e5;
 using i64 = long long;
@@ -10,23 +12,14 @@ using i64 = long long;
 
 
 
-//1.单点修改/赋值,区间/前缀和
+// 1.单点修改/赋值,区间/前缀和
 template <typename T = i64>
-class BIT1 {
+class Bit1 {
 public:
     int n;
     vector<T> bit, a;
-    BIT1(int n) {init(n);}
-    BIT1(const vector<T>& a) {init(a);}
-    void init(const int& _n) {
-        n = _n;
-        a.assign(n + 1, 0);
-        bit.assign(n + 1, 0);
-    }
-    void init(const vector<T>& _a) {
-        a = _a;
-        n = (a.size() - 1);
-        bit.assign(n + 1, 0);
+    Bit1(int n): n(n), a(n + 1), bit(n + 1) {}
+    Bit1(const vector<T>& a):a(a), n(a.size() - 1), bit(n + 1) {
         for(int i = 1; i <= n; i++) {
             bit[i] += a[i];
             int j = i + lowbit(i);
@@ -71,23 +64,14 @@ public:
     }
 };
 
-//2.区间修改, 区间和
+// 2.区间修改, 区间和
 template <class T = i64>
-class BIT2 {
+class Bit2 {
 public:
     int n;
     vector<T> bit1, bit2;
-    BIT2(int n) {init(n);}
-    BIT2(const vector<T>& a){init(a);}
-    void init(const int& _n) {
-        n = _n;
-        bit1.assign(n + 2, 0);
-        bit2.assign(n + 2, 0);
-    }
-    void init(const vector<T>& a) {
-        n = a.size() - 1;
-        bit1.assign(n + 2, 0);
-        bit2.assign(n + 2, 0);
+    Bit2(int n): n(n), bit1(n + 2), bit2(n + 2) {}
+    Bit2(const vector<T>& a): n(a.size() - 1), bit1(n + 2), bit2(n + 2) {
         for(int i = 1; i <= n; i++) {
             range_add(i, i, a[i]);
         }
@@ -123,3 +107,76 @@ public:
     }
 };
 
+// 3.二维树状数组
+template <typename T = i64>
+class Bit3 {
+public:
+    vector<int> xs;
+    vector<vector<int>> ys;
+    vector<vector<T>> bit;
+    int n;
+    Bit3(const vector<pair<int, int>> &pts = {}) {
+        for(const auto &[x, y] : pts) {
+            xs.emplace_back(x);
+        }
+        sort(xs.begin(), xs.end());
+        xs.erase(unique(xs.begin(), xs.end()), xs.end());
+        n = xs.size();
+        ys.assign(n + 1, {});
+        for(const auto &[x, y] : pts) {
+            int px = lower_bound(xs.begin(), xs.end(), x) - xs.begin() + 1;
+            while(px <= n) {
+                ys[px].emplace_back(y);
+                px += lower_bit(px);
+            }
+        }
+        bit.resize(n + 1);
+        for(int px = 1; px <= n; px++) {
+            auto &v = ys[px];
+            sort(v.begin(), v.end());
+            v.erase(unique(v.begin(), v.end()), v.end());
+            bit[px].assign(v.size() + 1, 0);
+        }
+    }
+    int lower_bit(int p) {
+        return p & -p;
+    }
+    void add(int x, int y, T v) {
+        auto it = lower_bound(xs.begin(), xs.end(), v);
+        assert(it != xs.end() && *it == x);
+        int px = it - xs.begin() + 1;
+        while(px <= n) {
+            it = lower_bound(ys[px].begin(), ys[px].end(), y);
+            assert(it != ys[px].end() && *it == y);
+            int py = it - ys[px].begin() + 1;
+            while(py < bit[px].size()) {
+                bit[px][py] += v;
+                py += lower_bit(py);
+            }
+            px += lower_bit(px);
+        }
+    }
+    void add(int x1, int y1, int x2, int y2, T v) {
+        assert(x1 <= x2 && y1 <= y2);
+        add(x1, y1, v);
+        add(x1, y2 + 1, -v);
+        add(x2 + 1, y1, -v);
+        add(x2 + 1, y2 + 1, v);
+    }
+    T sum(int x, int y) const{
+        T ans = 0;
+        int px = lower_bound(xs.begin(), xs.end(), x) - xs.begin();
+        while(px > 0) {
+            int py = lower_bound(ys[px].begin(), ys[px].end(), y) - ys[px].begin();
+            while(py > 0) {
+                ans += bit[px][py];
+                py -= lower_bit(py);
+            }
+            px -= lower_bit(px);
+        }
+        return ans;
+    }
+    T qry(int x, int y) const {
+        return sum(x + 1, y + 1);
+    }
+};
